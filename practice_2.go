d47c3212ebaf6571e1948f966781ea53af4a1dd7
udodinho <udodinho200@gmail.com>
2021-09-17 18:22:32 +0100

    working with interface

--- practice_2.go
+++ practice_2.go
@@ -1,32 +1,53 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"sync"
-)
+import "fmt"
 
-var wg sync.WaitGroup
+//var wg sync.WaitGroup
+//
+//func main() {
+//	fmt.Println("CPU Begin",runtime.NumCPU())
+//	fmt.Println("GOROUTINE Begin",runtime.NumGoroutine())
+//	wg.Add(2)
+//	go hear()
+//	go speak()
+//	fmt.Println("CPU middle",runtime.NumCPU())
+//	fmt.Println("GOROUTINE middle",runtime.NumGoroutine())
+//	wg.Wait()
+//	fmt.Println("CPU end",runtime.NumCPU())
+//	fmt.Println("GOROUTINE end",runtime.NumGoroutine())
+//}
+//
+//func speak()  {
+//	fmt.Println("Hello you are there?")
+//	wg.Done()
+//}
+//
+//func hear()  {
+//	fmt.Println("Yes am here!")
+//	wg.Done()
+//}
+
+
+type Human interface {
+	Speak()
+}
+
+type Person struct {
+	name string
+}
+
+func (p *Person) Speak()  {
+	fmt.Println("Hello are you there?")
+}
+
+func saySomething(h Human) {
+	h.Speak()
+}
 
 func main() {
-	fmt.Println("CPU Begin",runtime.NumCPU())
-	fmt.Println("GOROUTINE Begin",runtime.NumGoroutine())
-	wg.Add(2)
-	go hear()
-	go speak()
-	fmt.Println("CPU middle",runtime.NumCPU())
-	fmt.Println("GOROUTINE middle",runtime.NumGoroutine())
-	wg.Wait()
-	fmt.Println("CPU end",runtime.NumCPU())
-	fmt.Println("GOROUTINE end",runtime.NumGoroutine())
-}
-
-func speak()  {
-	fmt.Println("Hello you are there?")
-	wg.Done()
-}
-
-func hear()  {
-	fmt.Println("Yes am here!")
-	wg.Done()
+	p1 := Person{
+		name: "Goodness",
+	}
+	p1.Speak()
+	saySomething(&p1)
 }
